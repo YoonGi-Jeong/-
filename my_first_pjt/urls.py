1ee7f382e2b82f419779e66acca3df278b3ef579
JeongYoonGi <jygmyjesus@nate.com>
2024-04-19 11:13:33 +0900

    회원정보 수정

--- my_first_pjt/urls.py
+++ my_first_pjt/urls.py
@@ -21,7 +21,7 @@ from articles import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', views.index, name="index"),
+    path('', views.index, name="index"),
     path('articles/', include('articles.urls')),
     path('users/', include('users.urls')),
     path('accounts/',include('accounts.urls')),
